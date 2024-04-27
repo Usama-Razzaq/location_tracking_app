ed840d6dcb462cbce8c19e85b159e00dfbbd3691
Usama Razzaq <80523457+Usama-Razzaq@users.noreply.github.com>
2024-04-27 22:31:33 +0500

    added marker to the map

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,19 +15,68 @@ class _HomeScreenState extends State<HomeScreen> {
   Completer<GoogleMapController> _controller= Completer();
 
   static final CameraPosition _kGooglePlex = const CameraPosition(
-      target: LatLng(31.506432,  74.324378),
+      target: LatLng(31.432881646554577, 73.06686716900137),
     zoom: 14.4746,
   );
 
   List<Marker> _marker = [];
-  List<Marker> _list = const [
+  final List<Marker> _list = const [
     Marker(
       markerId: MarkerId('1'),
-      position: LatLng(31.506432, 74.324378),
+      position: LatLng(31.432881646554577, 73.06686716900137),
       infoWindow: InfoWindow(
-        title: 'My Current Location',
+        title: 'Zero Point Uaf',
       )
-    )
+    ),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(31.43446004169222, 73.06751631507443),
+        infoWindow: InfoWindow(
+          title: 'University Market',
+        )//31.43774689368027, 73.07146625925047
+    ),
+    Marker(
+        markerId: MarkerId('3'),
+        position: LatLng(31.43774689368027, 73.07146625925047),
+        infoWindow: InfoWindow(
+          title: 'Fateh Hall, Uaf',
+        )
+    ),
+    Marker(
+        markerId: MarkerId('4'),
+        position: LatLng(31.437833855897452, 73.07526426689694),
+        infoWindow: InfoWindow(
+          title: 'IBMS Gate',
+        )
+    ),
+    Marker(
+        markerId: MarkerId('5'),
+        position: LatLng(31.429433582405007, 73.06660785067267),
+        infoWindow: InfoWindow(
+          title: 'Post Office Chowk',
+        )
+    ),
+    Marker(
+        markerId: MarkerId('6'),
+        position: LatLng(31.426861174858203, 73.06392544100181),
+        infoWindow: InfoWindow(
+          title: 'GP Gate, UAF',
+        )
+    ),
+    Marker(
+        markerId: MarkerId('7'),
+        position: LatLng(31.428615391069933, 73.07544430440495),
+        infoWindow: InfoWindow(
+          title: 'Main Gate, UAF',
+        )
+    ),
+    Marker(
+        markerId: MarkerId('8'),
+        position: LatLng(31.432923405133163, 73.07015091718509),
+        infoWindow: InfoWindow(
+          title: 'UAF Clock Tower',
+        )
+    ),
   ];
 @override
 void initState(){
